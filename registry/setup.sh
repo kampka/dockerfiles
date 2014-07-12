abb62889dd3430ffc8746c0741e0e3811f5363f3
Christian Kampka <christian@kampka.net>
2014-07-12 22:30:31 +0200

    Registry version bump to 0.7.3

--- registry/setup.sh
+++ registry/setup.sh
@@ -1,25 +1,22 @@
 #!/bin/sh
 
-ln -sf /proc/self/fd /dev/fd
+ln -sf /proc/self/fd /dev
 
-yaourt -Sy python2-pip git gunicorn-python2 --noconfirm
-
-ln -sf /usr/bin/gunicorn-python2 /usr/local/bin/gunicorn
-
-cd /opt
+yaourt -Sy python2-pip git --noconfirm
+cd /tmp
 
 git clone https://github.com/dotcloud/docker-registry.git
 
 cd docker-registry
 
-git checkout 0.7.1
+git checkout 0.7.3
 
 pip2 install ./depends/docker-registry-core
 pip2 install .
 
 cd
 
-rm -rf /opt/docker-registry
+rm -rf /tmp/docker-registry
 rm -rf /build
 
 pacman -S -cc --noconfirm
