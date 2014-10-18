29bddda2faed4a15972f155d788534a04f6266ed
Christian Kampka <christian@kampka.net>
2014-10-18 14:31:19 +0200

    Update docker regsitry to versio 0.8.1 and simplify storage

--- registry/setup.sh
+++ registry/setup.sh
@@ -9,12 +9,14 @@ git clone https://github.com/dotcloud/docker-registry.git
 
 cd docker-registry
 
-git checkout 0.7.3
+git checkout 0.8.1
 
 pip2 install ./depends/docker-registry-core
 pip2 install .
 
 cd
+
+cp /build/config/config.yml /etc
 
 rm -rf /tmp/docker-registry
 rm -rf /build
