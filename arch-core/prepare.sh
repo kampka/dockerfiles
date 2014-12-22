48ad93157a9643ad59aa0870ad16b6983a52ac28
Christian Kampka <christian@kampka.net>
2014-12-22 11:59:43 +0100

    [arch-core] Move pacman keyring initialization into prepare step

--- arch-core/prepare.sh
+++ arch-core/prepare.sh
@@ -13,7 +13,32 @@ CMD_PREFIX=""
 trap "$CMD_PREFIX rm -rf $BUILD_DIR" EXIT
 
 
-$CMD_PREFIX pacstrap -c -d -G "$BUILD_DIR" filesystem bash pacman haveged
+$CMD_PREFIX pacstrap -c -d -G "$BUILD_DIR" filesystem bash pacman haveged sed
+#$CMD_PREFIX arch-chroot "$BUILD_DIR" /bin/bash -c "pacman-key --init"
+
+$CMD_PREFIX arch-chroot "$BUILD_DIR" /bin/bash <<'EOF'
+
+# Inside chroot
+
+haveged -w 4096
+
+rm -fr /etc/pacman.d/gnupg
+pacman-key --init
+pacman-key --populate archlinux
+
+pacman -Rs --noconfirm haveged
+ln -s /usr/share/zoneinfo/UTC /etc/localtime
+
+echo 'en_US.UTF-8 UTF-8' > /etc/locale.gen
+locale-gen
+
+echo "Server = https://mirrors.kernel.org/archlinux/\$repo/os/\$arch" > /etc/pacman.d/mirrorlist
+
+sed -i "s/^CheckSpace/#CheckSpace/g" /etc/pacman.conf
+
+EOF
+
+# Outside chroot
 
 cd "$BUILD_DIR"
 
--- arch-core/setup.sh
+++ arch-core/setup.sh
@@ -1,19 +1,5 @@
-#!/bin/sh
+#!/usr/bin/sh
 set -e
-
-
-haveged -w 4096
-pacman-key --init
-pacman -Rs --noconfirm haveged
-pacman-key --populate archlinux
-ln -s /usr/share/zoneinfo/UTC /etc/localtime
-
-echo 'en_US.UTF-8 UTF-8' > /etc/locale.gen
-locale-gen
-
-echo "Server = https://mirrors.kernel.org/archlinux/\$repo/os/\$arch" > /etc/pacman.d/mirrorlist
-
-sed -i "s/^CheckSpace/#CheckSpace/g" /etc/pacman.conf
 
 pacman -Syu --noconfirm
 
