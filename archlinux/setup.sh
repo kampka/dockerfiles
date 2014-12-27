4d2fdd0d6208f86c303c833407dd242d7c91f152
Christian Kampka <christian@kampka.net>
2014-12-27 16:26:20 +0100

    [archlinux] Change base image to arch-core and remove base-devel from default installation

--- archlinux/setup.sh
+++ archlinux/setup.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 set -e
 
-sed -i "s/^CheckSpace/#CheckSpace/g" /etc/pacman.conf
 cat >>/etc/pacman.conf<< EOF
 [kampka]
 Server = http://pkg.kampka.net
@@ -11,7 +10,6 @@ EOF
 pacman-key -a /build/pacman.d/kampka.db.key
 pacman-key --lsign 10C65A0F
 pacman -Sy -qq > /dev/null
-pacman -S --noconfirm base-devel >/dev/null
 
 pacman -S --noconfirm yaourt pacaur micro-init runit >/dev/null
 
