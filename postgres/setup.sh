69aa77171b46884a4fa0c90f4bd63ea16d5c226f
Christian Kampka <christian@kampka.net>
2015-01-25 22:23:32 +0100

    [postgres] Perform auto-upgrade on container startup

--- postgres/setup.sh
+++ postgres/setup.sh
@@ -4,9 +4,22 @@ set -e
 
 ln -sf /proc/self/fd /dev
 
-pacman -Sy --noconfirm postgresql postgresql-old-upgrade
+pacman -Sy --noconfirm --needed postgresql postgresql-old-upgrade grep
+
+mkdir -p /data
+
+mkdir -p /etc/initrc.d
+cp -v /build/initrc.d/* /etc/initrc.d
+
+mkdir -p /services
+cp -rv /build/services/* /services
 
 rm -rf /build
 
+REMOVABLES=$(pacman -Qqtd) || true
+
+if [ -n "$REMOVABLES" ]; then
+   pacman -Rns --noconfirm $REMOVABLES
+fi
 pacman -S -cc --noconfirm
 rm -rf /var/cache/pacman/pkg
